f5011adff1ad4a8cbeac1859cefdb36dccd1a004
Javier Smooth <iamjaviersmooth@gmail.com>
2015-08-23 13:50:19 -0700

    Improved slow-hash performance on non-AESNI

--- src/crypto/slow-hash.c
+++ src/crypto/slow-hash.c
@@ -36,6 +36,7 @@
 #include "common/int-util.h"
 #include "hash-ops.h"
 #include "oaes_lib.h"
+#include "aesb.h"
 
 #if defined(__x86_64__) || (defined(_MSC_VER) && defined(_WIN64))
 // Optimised code below, uses x86-specific intrinsics, SSE2, AES-NI
@@ -120,8 +121,6 @@
 #define THREADV __thread
 #endif
 
-extern int aesb_single_round(const uint8_t *in, uint8_t*out, const uint8_t *expandedKey);
-extern int aesb_pseudo_round(const uint8_t *in, uint8_t *out, const uint8_t *expandedKey);
 
 #pragma pack(push, 1)
 union cn_slow_hash_state
