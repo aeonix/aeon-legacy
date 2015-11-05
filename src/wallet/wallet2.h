140a1db71cf2df17440dd897cd1d9b1bac299d52
Javier Smooth <iamjaviersmooth@gmail.com>
2015-11-04 23:22:40 -0800

    Do not create transactions larger than the size of the penalty free zone In order to observe block size pressure due to transactions volumes, it is helpful to keep individual transaction sizes somewhat smaller so multiple transactions are more likely to be packed into a block.

--- src/wallet/wallet2.h
+++ src/wallet/wallet2.h
@@ -142,7 +142,7 @@ namespace tools
     // free block size. TODO: fix this so that it actually takes
     // into account the current median block size rather than
     // the minimum block size.
-    void init(const std::string& daemon_address = "http://localhost:8080", uint64_t upper_transaction_size_limit = ((CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE * 125) / 100) - CRYPTONOTE_COINBASE_BLOB_RESERVED_SIZE);
+    void init(const std::string& daemon_address = "http://localhost:8080", uint64_t upper_transaction_size_limit = CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE - CRYPTONOTE_COINBASE_BLOB_RESERVED_SIZE);
     bool deinit();
 
     void stop() { m_run.store(false, std::memory_order_relaxed); }
